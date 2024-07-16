5bae4ac282ce42ea7189e7f8d92823f775d5fffe
m.shvets <miss.shvets@gmail.com>
2024-07-16 15:23:12 +0200

    fix

--- front/src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ front/src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -64,7 +64,7 @@ export const LoginForm: React.FC<LoginFormProps> = memo((props) => {
         type: FormType.TEXT,
         defaultValue: username,
         value: username,
-        placeholder: 'Логин или email',
+        placeholder: 'Логин',
         onChange: onChangeUsername,
         rules: {
           required: true,
